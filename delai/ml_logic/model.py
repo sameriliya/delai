f81db3cad5506c3226ed5ea21ffd27d80dfdf8e5
Samer Iliya <sameriliya@gmail.com>
2022-12-01 19:42:48 +0000

    initial steps at mlops work for model

--- delai/ml_logic/model.py
+++ delai/ml_logic/model.py
@@ -1,4 +1,31 @@
-# this is a placeholder for the ML model
-# if it starts getting too big, consider breaking up the
-# model logic into different .py files inn the ml_logic folder
-# as we did in the challenges for ML_Ops
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import SGDClassifier
+import pandas as pd
+import numpy as np
+
+def initialize_model():
+    '''Initialize the model and return model variable'''
+    model = SGDClassifier()
+    return model
+
+def compile_model(model):
+    '''Needed for if we use a DL model.
+    For now, this is blank
+    '''
+    return model
+
+def train_model(model, X, y,
+                batch_size=64, patience=5, validation_split=0.3):
+    """
+    Fit model and return fitted_model
+    IF USING DL, update to:
+    Fit model and return a the tuple (fitted_model, history)
+    """
+    fitted_model = model.partial_fit(X,y)
+    return fitted_model
+
+def evaluate_model(model, X_val, y_val):
+    '''Pass validation data into model and evaluate performance metrics
+    '''
+
+    pass
---
+++ delai/ml_logic/registry.py
@@ -0,0 +1,11 @@
+#### Will be needed to save and load models locally and to the cloud (Mlflow, GCP)
+
+def save_model():
+    pass
+
+def load_model():
+    pass
+
+def get_model_version():
+    '''This is purely for mlflow to get model version from staging, production etc'''
+    pass
